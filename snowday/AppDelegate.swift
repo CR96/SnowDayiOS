ad3fbf191401121d0b662824057af20b630feeb7
Corey Rowe <CoreyR1196@gmail.com>
2016-08-18 16:11:11 -0400

    Install Crashlytics and Twitter

--- snowday/AppDelegate.swift
+++ snowday/AppDelegate.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
+import TwitterKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+
+        // TODO: Add Fabric API keys to Info.plist and the run script before shipping.
+        Fabric.with([Crashlytics.self, Twitter.self])
         return true
     }
 
